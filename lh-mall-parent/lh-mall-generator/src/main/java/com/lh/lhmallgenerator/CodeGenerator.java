a4ba5b3ebcd8e8856e6ddfdea50b2933e3615bd9
lhmeng1 <992610900@qq.com>
2022-05-23 23:15:29 +0800

    代码生成工具前的commit

--- lh-mall-parent/lh-mall-generator/src/main/java/com/lh/lhmallgenerator/CodeGenerator.java
+++ lh-mall-parent/lh-mall-generator/src/main/java/com/lh/lhmallgenerator/CodeGenerator.java
@@ -51,7 +51,7 @@ public class CodeGenerator {
 
         // 数据源配置
         DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setUrl("jdbc:mysql://localhost:3306/ant?useUnicode=true&useSSL=false&characterEncoding=utf8");
+        dsc.setUrl("jdbc:mysql://http://101.34.7.58/:33061/mall?useUnicode=true&useSSL=false&characterEncoding=utf8");
         // dsc.setSchemaName("public");
         dsc.setDriverName("com.mysql.jdbc.Driver");
         dsc.setUsername("root");
