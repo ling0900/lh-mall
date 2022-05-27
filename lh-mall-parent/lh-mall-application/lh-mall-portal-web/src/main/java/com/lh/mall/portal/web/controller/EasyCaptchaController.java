f904f28eb76dc51b38d8d929765b5968bbecd382
lhmeng1 <992610900@qq.com>
2022-05-27 20:53:24 +0800

    easy-Captcha 验证码校验
    Signed-off-by: lhmeng1 <992610900@qq.com>

--- lh-mall-parent/lh-mall-application/lh-mall-portal-web/src/main/java/com/lh/mall/portal/web/controller/EasyCaptchaController.java
+++ lh-mall-parent/lh-mall-application/lh-mall-portal-web/src/main/java/com/lh/mall/portal/web/controller/EasyCaptchaController.java
@@ -3,33 +3,33 @@ package com.lh.mall.portal.web.controller;
 import com.ramostear.captcha.HappyCaptcha;
 import com.ramostear.captcha.support.CaptchaStyle;
 import com.ramostear.captcha.support.CaptchaType;
+import com.wf.captcha.utils.CaptchaUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
-@RequestMapping("/happy-Captcha")
-public class HappyCaptchaController {
-
-    String attrName = "verifyCode";
+@RequestMapping("/easy-Captcha")
+public class EasyCaptchaController {
 
     /**
      * Generate code.
      */
     @RequestMapping("/generateCode")
     public void generateCode(HttpServletRequest request, HttpServletResponse response) {
-        HappyCaptcha.require(request, response)
-                .style(CaptchaStyle.ANIM)
-                // 设置样式，例如加减等等。
-                .type(CaptchaType.ARITHMETIC_ZH)
-                .build().finish();
+        try {
+            CaptchaUtil.out(request, response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @RequestMapping("/verifyCode")
     public String verifyCode(String code, HttpServletRequest request) {
-        Boolean aBoolean = HappyCaptcha.verification(request, code, true);
+        Boolean aBoolean = CaptchaUtil.ver(code, request);
         if (aBoolean) {
             // 可以这里验证后，直接remove掉。
             return "通过";
