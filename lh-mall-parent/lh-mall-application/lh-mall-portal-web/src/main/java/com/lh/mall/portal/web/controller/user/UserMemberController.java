ccfeab827df0bb2001a0a4925dd90adfd8536db8
lhmeng1 <992610900@qq.com>
2022-05-28 23:30:44 +0800

    阿里巴巴，谷歌代码排版。。。
    Signed-off-by: lhmeng1 <992610900@qq.com>

--- lh-mall-parent/lh-mall-application/lh-mall-portal-web/src/main/java/com/lh/mall/portal/web/controller/user/UserMemberController.java
+++ lh-mall-parent/lh-mall-application/lh-mall-portal-web/src/main/java/com/lh/mall/portal/web/controller/user/UserMemberController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import javax.validation.Valid;
 
 /**
@@ -21,19 +20,32 @@ import javax.validation.Valid;
 @RequestMapping("/user-member")
 public class UserMemberController {
 
-//    @Autowired
-    //@Resource 这里得将他的实现jar包引进来到pom。
-    // 这里需要将service和对应的impl一起导入包！
+    /**
+     * @Resource 这里得将他的实现jar包引进来到pom。 这里需要将service和对应的impl一起导入包！
+     */
     @Autowired
     UmsMemberService umsMemberService;
 
     @RequestMapping("/test")
-    public void test(){
+    public void test() {
         UmsMember umsMember = new UmsMember();
         umsMember.getNickName().toString();
     }
 
+    /**
+     * 获取验证码
+     *
+     * @return 验证码
+     */
+    @RequestMapping("getCaptcha")
+    public String getCaptcha() {
+        return "";
+    }
 
+    /**
+     * @param umsMemberRegisterDTO 验证码、用户信息
+     * @return
+     */
     @RequestMapping("/register")
     public ResultWrapper register(@RequestBody @Valid UmsMemberRegisterDTO umsMemberRegisterDTO) {
         umsMemberService.register(umsMemberRegisterDTO);
@@ -46,7 +58,12 @@ public class UserMemberController {
         return "登陆结果: " + login;
     }
 
-    // 思考如何针对字段接口定制校验字段？
+    /**
+     * 思考如何针对字段接口定制校验字段？
+     *
+     * @param umsMemberLoginDTO
+     * @return
+     */
     @RequestMapping("/verifyToken")
     public String verifyToken(@RequestBody UmsMemberLoginDTO umsMemberLoginDTO) {
         boolean b = umsMemberService.verifyToken(umsMemberLoginDTO);
--- lh-mall-parent/lh-mall-application/lh-mall-portal-web/src/main/java/com/lh/mall/portal/web/cutom/MyCustomGoogleKaptcha.java
+++ lh-mall-parent/lh-mall-application/lh-mall-portal-web/src/main/java/com/lh/mall/portal/web/cutom/MyCustomGoogleKaptcha.java
@@ -1,17 +1,13 @@
 /**
- * Copyright © 2018 TaoYu (tracy5546@gmail.com)
- * modify by 孟令浩 992610900@qq.com
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright © 2018 TaoYu (tracy5546@gmail.com) modify by 孟令浩 992610900@qq.com Licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package com.lh.mall.portal.web.cutom;
 
@@ -36,79 +32,76 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
- * 谷歌默认验证码组件 个性化定制
- * redis 个性化定制 by 孟令浩
+ * 谷歌默认验证码组件 个性化定制 redis 个性化定制 by 孟令浩
  * @author TaoYu
  */
 @Slf4j
 @Component
 public class MyCustomGoogleKaptcha implements Kaptcha {
 
-  public static final String KAPTCHA_SESSION_KEY = "KAPTCHA_SESSION_KEY";
-  public static final String KAPTCHA_SESSION_DATE = "KAPTCHA_SESSION_DATE";
+    public static final String KAPTCHA_SESSION_KEY = "KAPTCHA_SESSION_KEY";
+    public static final String KAPTCHA_SESSION_DATE = "KAPTCHA_SESSION_DATE";
 
-  @Autowired
-  private RedisTemplate<String, Object> redisTemplate;
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
-  private DefaultKaptcha kaptcha;
+    private DefaultKaptcha kaptcha;
 
-  @Autowired
-  private HttpServletRequest request;
+    @Autowired
+    private HttpServletRequest request;
 
-  @Autowired
-  private HttpServletResponse response;
+    @Autowired
+    private HttpServletResponse response;
 
-
-
-  public MyCustomGoogleKaptcha(DefaultKaptcha kaptcha) {
-    this.kaptcha = kaptcha;
-  }
-
-  @Override
-  public String render() {
-    response.setDateHeader(HttpHeaders.EXPIRES, 0L);
-    response.setHeader(HttpHeaders.CACHE_CONTROL, "no-store, no-cache, must-revalidate");
-    response.addHeader(HttpHeaders.CACHE_CONTROL, "post-check=0, pre-check=0");
-    response.setHeader(HttpHeaders.PRAGMA, "no-cache");
-    response.setContentType("image/jpeg");
-    String sessionCode = kaptcha.createText();
-    try (ServletOutputStream out = response.getOutputStream()) {
-      redisTemplate.opsForValue().set(KAPTCHA_SESSION_KEY + ":" + request.getSession().getId(), sessionCode);
-      redisTemplate.opsForValue().set(KAPTCHA_SESSION_DATE + ":" + request.getSession().getId(), System.currentTimeMillis());
-      ImageIO.write(kaptcha.createImage(sessionCode), "jpg", out);
-      return sessionCode;
-    } catch (IOException e) {
-      throw new KaptchaRenderException(e);
+    public MyCustomGoogleKaptcha(DefaultKaptcha kaptcha) {
+        this.kaptcha = kaptcha;
     }
-  }
 
-  @Override
-  public boolean validate(String code) {
-    return validate(code, 900);
-  }
-
-  @Override
-  public boolean validate(@NonNull String code, long second) {
-    HttpSession httpSession = request.getSession(false);
-    String sessionCode;
-    if (httpSession != null && (sessionCode = (String) httpSession.getAttribute(KAPTCHA_SESSION_KEY)) != null) {
-      if (sessionCode.equalsIgnoreCase(code)) {
-        long sessionTime = (long) redisTemplate.opsForValue().get(KAPTCHA_SESSION_DATE + ":" + request.getSession().getId());
-        long duration = (System.currentTimeMillis() - sessionTime) / 1000;
-        if (duration < second) {
-          redisTemplate.delete(KAPTCHA_SESSION_KEY + ":" + request.getSession().getId());
-          redisTemplate.delete(KAPTCHA_SESSION_DATE + ":" + request.getSession().getId());
-          return true;
-        } else {
-          throw new KaptchaTimeoutException();
+    @Override
+    public String render() {
+        response.setDateHeader(HttpHeaders.EXPIRES, 0L);
+        response.setHeader(HttpHeaders.CACHE_CONTROL, "no-store, no-cache, must-revalidate");
+        response.addHeader(HttpHeaders.CACHE_CONTROL, "post-check=0, pre-check=0");
+        response.setHeader(HttpHeaders.PRAGMA, "no-cache");
+        response.setContentType("image/jpeg");
+        String sessionCode = kaptcha.createText();
+        try (ServletOutputStream out = response.getOutputStream()) {
+            redisTemplate.opsForValue().set(KAPTCHA_SESSION_KEY + ":" + request.getSession().getId(), sessionCode);
+            redisTemplate.opsForValue().set(KAPTCHA_SESSION_DATE + ":" + request.getSession().getId(),
+                System.currentTimeMillis());
+            ImageIO.write(kaptcha.createImage(sessionCode), "jpg", out);
+            return sessionCode;
+        } catch (IOException e) {
+            throw new KaptchaRenderException(e);
         }
-      } else {
-        throw new KaptchaIncorrectException();
-      }
-    } else {
-      throw new KaptchaNotFoundException();
     }
-  }
 
+    @Override
+    public boolean validate(String code) {
+        return validate(code, 900);
+    }
+
+    @Override
+    public boolean validate(@NonNull String code, long second) {
+        HttpSession httpSession = request.getSession(false);
+        String sessionCode;
+        if (httpSession != null && (sessionCode = (String)httpSession.getAttribute(KAPTCHA_SESSION_KEY)) != null) {
+            if (sessionCode.equalsIgnoreCase(code)) {
+                long sessionTime =
+                    (long)redisTemplate.opsForValue().get(KAPTCHA_SESSION_DATE + ":" + request.getSession().getId());
+                long duration = (System.currentTimeMillis() - sessionTime) / 1000;
+                if (duration < second) {
+                    redisTemplate.delete(KAPTCHA_SESSION_KEY + ":" + request.getSession().getId());
+                    redisTemplate.delete(KAPTCHA_SESSION_DATE + ":" + request.getSession().getId());
+                    return true;
+                } else {
+                    throw new KaptchaTimeoutException();
+                }
+            } else {
+                throw new KaptchaIncorrectException();
+            }
+        } else {
+            throw new KaptchaNotFoundException();
+        }
+    }
 }
-
