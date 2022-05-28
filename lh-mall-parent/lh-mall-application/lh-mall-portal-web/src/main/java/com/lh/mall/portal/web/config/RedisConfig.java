28c7c0c4e0c8dcacbaa6e73115428d860a3afec6
lhmeng1 <992610900@qq.com>
2022-05-28 20:49:45 +0800

    RedisConfig配置
    Signed-off-by: lhmeng1 <992610900@qq.com>

--- lh-mall-parent/lh-mall-application/lh-mall-portal-web/src/main/java/com/lh/mall/portal/web/config/RedisConfig.java
+++ lh-mall-parent/lh-mall-application/lh-mall-portal-web/src/main/java/com/lh/mall/portal/web/config/RedisConfig.java
@@ -6,30 +6,33 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
+/**
+ * @author 99261
+ * 方法设置为公有的，要把哪个对象装配到容器当中，那么就返回这个对象，这边返回的RedisTemplate。
+ */
 @Configuration
 public class RedisConfig {
 
-    @Bean//定义第三方的Bean
-    //方法设置为公有的，要把哪个对象装配到容器当中，那么就返回这个对象，这边返回的RedisTemplate
-    //用Template访问数据库，Template要有访问数据库的能力，那他就得能够创建连接，连接由连接工厂创建，所以就要把连接工厂
-    //工厂放进来，注入给Template，他才能访问数据库(RedisConnectionFactory factory)就是声明连接工厂
+    @Bean
+    /**
+     redisTemplate要有访问redis数据库的能力，需要创建连接（工厂）注入给redisTemplate，
+     RedisConnectionFactory factory：声明连接工厂。
+    */
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory){
-        //实例化这个Bean
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        //把工厂设置给Template
-        template.setConnectionFactory(factory);
-        //配置Template主要配置序列化的方式，因为写的是java程序，得到的是java类型的数据，最终要这个数据存储到数据库里面
-        //就要指定一种序列化的方式，或者说数据转换的方式
-        //设置key的序列化方式
-        template.setKeySerializer(RedisSerializer.string());
-        //设置value的序列化方式
-        template.setValueSerializer(RedisSerializer.json());
-        //设置hash的key的序列化方式
-        template.setHashKeySerializer(RedisSerializer.string());
-        //设置hash的value的序列化方式
-        template.setHashValueSerializer(RedisSerializer.json());
-
-        template.afterPropertiesSet();//使上面参数生效
-        return template;
+        // 实例化这个Bean
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        // 把工厂设置给Template
+        redisTemplate.setConnectionFactory(factory);
+        /*
+        主要配置序列化的方式，得到的是java类型的数据和数据存储数据做交互，故需要指定序列化方式（数据转换的方式）。
+        */
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        redisTemplate.setValueSerializer(RedisSerializer.json());
+        redisTemplate.setHashKeySerializer(RedisSerializer.string());
+        redisTemplate.setHashValueSerializer(RedisSerializer.json());
+        // 使配置生效
+        redisTemplate.afterPropertiesSet();
+        // 返回
+        return redisTemplate;
     }
 }
